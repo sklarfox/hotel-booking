a14ec17c4d3df7f316997ad4f90a18fd416c3151
Alex Sklar <sklarfox@gmail.com>
2024-09-09 14:03:26 -0700

    feat(api): create validateRequestDates helper

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -40,3 +40,14 @@ export const checkAvailability = async (
 
   return !booking
 }
+
+export const validateRequestDates = (reqCheckIn, reqCheckOut) => {
+  let today = new Date()
+  today = new Date(today.getFullYear(), today.getMonth(), today.getDay())
+  const checkInDate = new Date(reqCheckIn)
+  const checkOutDate = new Date(reqCheckOut)
+
+  const isInFuture = checkInDate >= today
+  const minimumOneDay = checkOutDate > checkInDate
+  return isInFuture && minimumOneDay
+}
