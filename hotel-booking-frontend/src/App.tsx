feadae882105c1f55706a2cd431fb71d473a73b1
Alex Sklar <sklarfox@gmail.com>
2024-09-11 11:52:20 -0700

    feat(ui): add CardGrid and Card components

--- hotel-booking-frontend/src/App.tsx
+++ hotel-booking-frontend/src/App.tsx
@@ -1,40 +1,20 @@
-import { DarkThemeToggle, Navbar, Button } from 'flowbite-react'
-import { useEffect, useState } from 'react'
-
-function Header() {
-  return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="">
-        <img
-          src="/hotel-icon.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Hotel Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Hotel Booking
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button className="mr-2">Book Now</Button>
-        <DarkThemeToggle />
-      </div>
-    </Navbar>
-  )
-}
+import { useState, useEffect } from 'react'
+import { CardGrid, Room } from './components/CardGrid'
+import { Header } from './components/Header'
 
 function App() {
-  const [rooms, setRooms] = useState(['room1', 'room2'])
+  const [rooms, setRooms] = useState<Room[]>([])
 
   useEffect(() => {
     fetch(import.meta.env.VITE_API_URL + 'rooms')
-      .then((response) => response.json())
-      .then((data) => setRooms(data))
-      .catch((error) => console.error('Error fetching rooms:', error))
+      .then(response => response.json())
+      .then(data => setRooms(data))
+      .catch(error => console.error('Error fetching rooms:', error))
   }, [])
   return (
     <>
       <Header></Header>
-      {rooms}
+      <CardGrid rooms={rooms}></CardGrid>
     </>
   )
 }
---
+++ hotel-booking-frontend/src/components/CardGrid.tsx
@@ -0,0 +1,47 @@
+import { Card, Button } from 'flowbite-react'
+
+export interface Room {
+  id: number
+  name: string
+  price: number
+  beds: number
+  description: string
+}
+
+const RoomCard = ({ room }: { room: Room }) => {
+  return (
+    <Card
+      className="m-2 max-w-sm"
+      imgSrc="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {room.name}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {room.description}
+      </p>
+      <div className="flex justify-evenly">
+        <div>
+          <span className="text-xl font-bold text-gray-900 dark:text-white">
+            ${room.price / 100}
+          </span>
+          <span className="text-gray-700 dark:text-gray-400"> / night</span>
+        </div>
+        <span className="text-lg font-bold text-gray-900 dark:text-white">
+          🛌 {room.beds} beds
+        </span>
+      </div>
+      <Button>🌴 Check Availability</Button>
+    </Card>
+  )
+}
+
+export const CardGrid = ({ rooms }: { rooms: Room[] }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      {rooms.map(room => (
+        <RoomCard key={room.id} room={room}></RoomCard>
+      ))}
+    </div>
+  )
+}
