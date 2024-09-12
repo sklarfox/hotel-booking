3b30b6b0a84f58d1fdb6c738eccce954f9c9da46
Alex Sklar <sklarfox@gmail.com>
2024-09-11 21:00:48 -0700

    feat(auth): add auth protection check to all routes

--- hotel-booking-frontend/src/App.tsx
+++ hotel-booking-frontend/src/App.tsx
@@ -1,21 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { CardGrid, Room } from './components/CardGrid'
 import Header from './components/Header'
 import BookingRoute from './routes/BookingRoute'
 import BookingsRoute from './routes/BookingsRoute'
 import RoomsRoute from './routes/RoomsRoute'
-
-import { Button } from 'flowbite-react'
+import RequireAuth from './routes/RequireAuth'
+import Login from './components/Login'
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <Router>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<BookingRoute />} />
-        <Route path="/bookings" element={<BookingsRoute />} />
-        <Route path="/rooms" element={<RoomsRoute />} />
+        <Route
+          path="/"
+          element={user ? <></> : <Navigate replace to="/login" />}
+        />
+        <Route element={<RequireAuth user={user} />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/book" element={<BookingRoute />} />
+          <Route path="/bookings" element={<BookingsRoute />} />
+          <Route path="/rooms" element={<RoomsRoute />} />
+        </Route>
       </Routes>
     </Router>
   )
