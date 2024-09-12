ac660185c12ef6a90b9021639924bf21dbfc051a
Alex Sklar <sklarfox@gmail.com>
2024-09-12 15:00:55 -0700

    fix(db): enhance error handling in validDates checker to prevent uncaught exceptions with invalid data

--- backend/src/api/bookings.js
+++ backend/src/api/bookings.js
@@ -100,18 +100,31 @@ router.patch('/:id', async (req, res) => {
       .send('Booking not found. Please check the booking ID and try again.')
   }
 
-  const { checkInDate, checkOutDate } = req.body
+  let { checkInDate, checkOutDate } = req.body
+  checkInDate = checkInDate || booking.checkInDate
+  checkOutDate = checkOutDate || booking.checkOutDate
+
+  const validDates = validBookingDates(checkInDate, checkOutDate)
+
+  if (!validDates) {
+    return res
+      .status(400)
+      .send(
+        'Date Error: Invalid dates requested. Please verify the requested dates and try again.',
+      )
+  }
 
   const isAvailable = await checkRoomAvailability(
     booking.roomId,
-    checkInDate,
-    checkOutDate,
+    checkInDate || booking.checkInDate,
+    checkOutDate || booking.checkOutDate,
   )
 
   if (isAvailable) {
     try {
-      booking.checkInDate = checkInDate || booking.checkInDate
-      booking.checkOutDate = checkOutDate || booking.checkOutDate
+      booking.checkInDate = checkInDate
+      booking.checkOutDate = checkOutDate
+      console.log('saving', checkInDate, checkOutDate)
       await booking.save()
       return res.json(booking)
     } catch (error) {
--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -4,6 +4,12 @@ export const validBookingDates = (reqCheckIn, reqCheckOut) => {
   const checkInDate = new Date(reqCheckIn)
   const checkOutDate = new Date(reqCheckOut)
 
+  if (
+    Number.isNaN(checkInDate.getTime()) ||
+    Number.isNaN(checkOutDate.getTime())
+  ) {
+    return false
+  }
   const isInFuture = checkInDate >= today
   const minimumOneDay = checkOutDate > checkInDate
   return isInFuture && minimumOneDay
