bfdd90ace8c7f01194ca6e4d43a9d4fb074e3847
Alex Sklar <sklarfox@gmail.com>
2024-09-11 17:41:17 -0700

    refactor: move Alert and Modal to separate components

--- hotel-booking-frontend/src/App.tsx
+++ hotel-booking-frontend/src/App.tsx
@@ -2,95 +2,11 @@ import { useState, useEffect } from 'react'
 import { CardGrid, Room } from './components/CardGrid'
 import { Header } from './components/Header'
 import { DateSelector } from './components/DateSelector'
-import { getNumberOfNights, getTomorrow } from './utils/helpers'
+import { AlertBar } from './components/Alert'
+import { getTomorrow } from './utils/helpers'
+import { BookingModal } from './components/Modal'
 
-import { Alert, Modal, Button } from 'flowbite-react'
-
-const AlertBar = ({ alert }: { alert: string }) => {
-  if (alert.includes('confirmed')) {
-    return (
-      <Alert color="success" rounded>
-        <span className="font-medium">Success!</span> {alert}
-      </Alert>
-    )
-  } else {
-    return (
-      <Alert color="warning" rounded>
-        <span className="font-medium">Alert!</span> {alert}
-      </Alert>
-    )
-  }
-}
-
-interface BookingModalProps {
-  setAlert: React.Dispatch<React.SetStateAction<string>>
-  setRooms: React.Dispatch<React.SetStateAction<Room[]>>
-  showModal: boolean
-  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
-  checkIn: Date
-  checkOut: Date
-  room: Room | undefined
-}
-
-const BookingModal = ({
-  setAlert,
-  setRooms,
-  showModal,
-  setShowModal,
-  checkIn,
-  checkOut,
-  room,
-}: BookingModalProps) => {
-  if (room === undefined) {
-    return null
-  }
-
-  const handleReservationClick = async () => {
-    const response = await fetch(import.meta.env.VITE_API_URL + 'bookings', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        roomId: room.id,
-        clientEmail: 'hello@hello.com', // TODO remove hardcoded email
-        checkInDate: checkIn.toISOString().split('T')[0],
-        checkOutDate: checkOut.toISOString().split('T')[0],
-      }),
-    })
-
-    if (response.ok) {
-      setAlert('Your reservation is confirmed!')
-      setShowModal(false)
-      setRooms(prev => prev.filter(ele => ele !== room))
-    } else {
-      setAlert('Your reservation could not be booked. Please try again.')
-      setShowModal(false)
-    }
-  }
-  return (
-    <Modal dismissible show={showModal} onClose={() => setShowModal(false)}>
-      <Modal.Header>Book Your Room</Modal.Header>
-      <Modal.Body>
-        <div className="dark:text-white">
-          <p className="text-lg">
-            <strong>{room.name}</strong>
-          </p>
-          <p>Check in: {checkIn.toDateString()}</p>
-          <p>Check out: {checkOut.toDateString()}</p>
-          <p>Price: ${room.price / 100} / night</p>
-          <p className="text-lg">
-            Total: ${(getNumberOfNights(checkIn, checkOut) * room.price) / 100}
-          </p>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={handleReservationClick}>
-          🌴 Complete Reservation
-        </Button>
-        <Button onClick={() => setShowModal(false)}>Cancel</Button>
-      </Modal.Footer>
-    </Modal>
-  )
-}
+import { Button } from 'flowbite-react'
 
 function App() {
   const [rooms, setRooms] = useState<Room[]>([])
