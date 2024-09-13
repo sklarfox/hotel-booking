fb640c468f0e600a9671098f7f55ebcff15f00f3
Alex Sklar <sklarfox@gmail.com>
2024-09-13 08:59:39 -0700

    feat(docker): extract remaining postgres vars to env variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,11 +3,22 @@ import { Sequelize } from 'sequelize'
 
 dotenv.config()
 
-const sequelize = new Sequelize('booking-app', process.env.POSTGRES_USER, '', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-})
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+  },
+)
+console.log(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  typeof process.env.DB_HOST,
+)
 
 await sequelize
   .sync({ alter: true })
